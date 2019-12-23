36c16ae58e5d282336d61ebd51e429acea2d5ffd
Tomas <tleffew1994@gmail.com>
2019-12-23 12:00:40 -0700

    move user loader to its own file

--- graphql/dataloaders/dataloader.go
+++ graphql/dataloaders/dataloader.go
@@ -3,24 +3,14 @@ package dataloaders
 import (
 	"context"
 	"github.com/penthious/go-gql-meetup/graphql"
-	"github.com/penthious/go-gql-meetup/models"
 	"net/http"
-	"time"
 )
 
 const USER_LOADER_KEY = "userLoader"
 
 func DataloaderMiddleware(d *graphql.Domain, next http.Handler) http.Handler  {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userLoader := UserLoader{
-			maxBatch: 100,
-			wait: 1 * time.Millisecond,
-			fetch: func(ids []string) ([]*models.User, []error) {
-				users, err := d.DB.UserRepo.GetByIDs(ids)
-
-				return users, []error{err}
-			},
-		}
+		userLoader := CreateUserLoader(d)
 
 		ctx := context.WithValue(r.Context(), USER_LOADER_KEY, &userLoader)
 
--- graphql/dataloaders/userloader.go
+++ graphql/dataloaders/userloader.go
@@ -1,2 +1,34 @@
 package dataloaders
 
+import (
+	"github.com/penthious/go-gql-meetup/graphql"
+	"github.com/penthious/go-gql-meetup/models"
+	"time"
+)
+
+func CreateUserLoader(d *graphql.Domain) UserLoader {
+	return UserLoader{
+		maxBatch: 100,
+		wait:     1 * time.Millisecond,
+		fetch: func(ids []string) ([]*models.User, []error) {
+			users, err := d.DB.UserRepo.GetByIDs(ids)
+
+			if err != nil {
+				return nil, []error{err}
+			}
+
+			u := make(map[string]*models.User, len(users))
+			for _, user := range users {
+				u[user.ID] = user
+			}
+
+			result := make([]*models.User, len(ids))
+
+			for i, id := range ids {
+				result[i] = u[id]
+			}
+
+			return result, []error{err}
+		},
+	}
+}
