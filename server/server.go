26b0a316705da381b4b1ef385be97ca6377e4fbf
Tomas <tleffew1994@gmail.com>
2019-12-24 21:55:23 -0700

    Clean up code

--- server/server.go
+++ server/server.go
@@ -6,12 +6,12 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-pg/pg/v9"
 	"github.com/gorilla/websocket"
-	go_gql_meetup2 "github.com/penthious/go-gql-meetup"
+	"github.com/penthious/go-gql-meetup"
 	"github.com/penthious/go-gql-meetup/database"
 	"github.com/penthious/go-gql-meetup/domain"
 	"github.com/penthious/go-gql-meetup/domain/utils"
 	"github.com/penthious/go-gql-meetup/graphql/dataloaders"
-	go_gql_meetup "github.com/penthious/go-gql-meetup/graphql/resolvers"
+	go_gql_meetup_resolvers "github.com/penthious/go-gql-meetup/graphql/resolvers"
 	"net/http"
 )
 
@@ -35,13 +35,13 @@ func main() {
 	// See https://github.com/rs/cors for full option listing
 
 
-	c := go_gql_meetup.Config{
-		Resolvers: &go_gql_meetup.Resolver{Domain: *g},
+	c := go_gql_meetup_resolvers.Config{
+		Resolvers: &go_gql_meetup_resolvers.Resolver{Domain: *g},
 	}
 
-	go_gql_meetup2.SetDirectives(&c)
+	go_gql_meetup.SetDirectives(&c)
 
-	srv := handler.NewDefaultServer(go_gql_meetup.NewExecutableSchema(c))
+	srv := handler.NewDefaultServer(go_gql_meetup_resolvers.NewExecutableSchema(c))
 	srv.AddTransport(&transport.Websocket{
 		Upgrader: websocket.Upgrader{
 			CheckOrigin: func(r *http.Request) bool {
